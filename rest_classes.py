c70369897a0c70090c6e9c10b4f372563b1d1019
Chrisebell24 <31806186+Chrisebell24@users.noreply.github.com>
2021-08-13 11:02:00 -0500

    Create rest_classes.py

---
+++ rest_classes.py
@@ -0,0 +1,8 @@
+import json
+from flask_restful import Api, Resource, reqparse
+
+# REST API SECTION
+#class TestClass(Resource):
+#    def get(self, x):
+#        import json
+#        return json.dumps('test class '+str(x))
